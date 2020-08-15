27c9c607311f1fd10bb9e39a87a61688d0279d03
Eric Bailey <eric@ericb.me>
2020-08-15 14:11:51 -0500

    Remove unused nix/meta.nix

--- nix/meta.nix
+++
@@ -1,7 +0,0 @@
-{ stdenv, lilypond }:
-
-{
-  homepage = https://github.com/yurrriq/epbd-patience-is-a-virtual-ep;
-  maintainers = with stdenv.lib.maintainers; [ yurrriq ];
-  license = stdenv.lib.licenses.cc-by-nc-40;
-}
